7dc033affc58b0a7dea69f3eb1df15bb856692ae
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-03-24 14:19:35 -0300

    Adds login route

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const ongController = require('./controllers/ong-controller');
 const incidentController = require('./controllers/incident-controller');
 const profileController = require('./controllers/profile-controller');
+const sessionController = require('./controllers/session-controller');
 
 const routes = express.Router();
+
+routes.post('/sessions', sessionController.create)
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
