0b3a98208f6ab2fa03f41915dfa24b0f0a012b20
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-03-24 14:12:38 -0300

    Adds specific ong incidents listing route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const ongController = require('./controllers/ong-controller');
 const incidentController = require('./controllers/incident-controller');
+const profileController = require('./controllers/profile-controller');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
 
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
