5f3122c51b5ca07dca7c629299bbbf6374036d93
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-03-24 13:59:56 -0300

    Adds incidents creation and listing routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const ongController = require('./controllers/ong-controller');
+const incidentController = require('./controllers/incident-controller');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
 
 module.exports = routes;
